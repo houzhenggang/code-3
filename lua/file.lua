afdd992c48730f88e5ab0dcfb6229b144df46f45
xiaoxuanzi <xiaoximou@gmail.com>
2015-12-11 18:35:14 +0800

    lua file io test

--- lua/file.lua
+++ lua/file.lua
@@ -9,15 +9,34 @@ file:seek("set")
 buf, msg = file:read( size )
 print(buf)
 print(tostring(msg))
+print("--------------------")
+file:seek("set")
+local read_size = 0
+while true do
+    buf, msg = file:read( 3 )
+    print(i)
+    i = i + 1
+    print('buf = ', buf, 'size = ', string.len( buf ))
+    read_size = read_size  + string.len( buf )
+    print("read_size = ", read_size)
+    if buf ~= nil and size == read_size then
+        print("nil ok size = ", string.len( buf ) )
+        print("msg = ", msg )
+        break
+    end
+end
+
+--end
+
 --[[
 while true do
     buf, msg = file:read( 123 )
     print(i)
     i = i + 1
-    print('buf = ', buf)
+    print('buf = ', buf, 'size = ', string.len( buf ))
 
-    if buf == nil then
-        print('ERROR')
+    if buf == nil and string.len(buf) == size then
+        print('ok ',string.len(buf))
         print('msg = ',tostring(msg))
         break
     end
@@ -29,5 +48,4 @@ while true do
 
 end
 
-print(buf)
---]]
+---]]
--- lua/table.lua
+++ lua/table.lua
@@ -14,3 +14,20 @@ print("-------ipairs------")
 for key, val in ipairs(impl) do
     print(key, val)
 end
+print("-function with impl---")
+impl.get_print = function(impl, a, b)
+    --print("a and b",a,b)
+    return a,b
+end
+
+aa, bb = impl:get_print(1,2)
+print(aa, bb)
+
+print("-function without impl---")
+impl.get_print = function(a, b)
+    --print("a and b",a,b)
+    return a,b
+end
+
+aa, bb = impl.get_print(1,2)
+print(aa, bb)
